6152579369e44b3d8e7f898c3a54da25ab03c987
samanta evangelista <samidanieva@gmail.com>
2023-10-02 19:39:55 -0300

    se corrige errores de  sintaxis

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let productos = [
-    { id: 1, nombre: "alfajor jorgito negro", categoria: "golosinas", stock: 30, precio: 180, ruta de img: "jorgitonegro.jpg"}
-    { id: 2, nombre: "alfajor jorgito blanco", categoria: "golosinas", stock: 30, precio: 180, ruta de img: "jorgitoblanco.jpg"}
-    { id: 3, nombre: "tutucas", categoria: "snack", stock: 25, precio: 200, ruta de img: "tutucas.jpg"}
-    { id: 4, nombre: "pico dulce", categoria: "golosinas", stock: 50, precio: 150, ruta de img: "picodulce.jpg"}
-    { id: 5, nombre: "papas fritas", categoria: "snack", stock: 25, precio: 350, ruta de img: "papasfritas.jpg"}
-    { id: 6, nombre: "palitos salados", categoria: "snack", stock: 25, precio: 250, ruta de img: "palitossalados.jpg"}
-    { id: 7, nombre: "agua mineral", categoria: "bebidas", stock: 30, precio: 300, ruta de img: "aguamineral.jpg"}
-    { id: 8, nombre: "jugo baggio multifruta", categoria: "bebidas", stock: 50, precio: 200, ruta de img: "baggiomultifrutas.jpg"}
-    { id: 9, nombre: "jugo baggio naranja", categoria: "bebidas", stock: 50, precio: 200, ruta de img: "baggionaranja.jpg"}
+    { id: 1, nombre: "alfajor jorgito negro", categoria: "golosinas", stock: 30, precio: 180, ruta: "jorgitonegro.jpg" },
+    { id: 2, nombre: "alfajor jorgito blanco", categoria: "golosinas", stock: 30, precio: 180, ruta: "jorgitoblanco.jpg" },
+    { id: 3, nombre: "tutucas", categoria: "snack", stock: 25, precio: 200, ruta: "tutucas.jpg" },
+    { id: 4, nombre: "pico dulce", categoria: "golosinas", stock: 50, precio: 150, ruta: "picodulce.jpg" },
+    { id: 5, nombre: "papas fritas", categoria: "snack", stock: 25, precio: 350, ruta: "papasfritas.jpg" },
+    { id: 6, nombre: "palitos salados", categoria: "snack", stock: 25, precio: 250, ruta: "palitossalados.jpg" },
+    { id: 7, nombre: "agua mineral", categoria: "bebidas", stock: 30, precio: 300, ruta: "aguamineral.jpg" },
+    { id: 8, nombre: "jugo baggio multifruta", categoria: "bebidas", stock: 50, precio: 200, ruta: "baggiomultifrutas.jpg" },
+    { id: 9, nombre: "jugo baggio naranja", categoria: "bebidas", stock: 50, precio: 200, ruta: "baggionaranja.jpg" },
 ]
 
 
